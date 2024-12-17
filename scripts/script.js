4f5001d33f45e7b505d90c616701d528f8e3c8f7
genialingele <ingelegenial@gmail.com>
2024-12-17 23:16:28 +0200

    INIT COMMIT

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+/*
+  Usage Rights: You are free to edit, copy, and use this website template for personal or commercial projects. 
+  However, you are not allowed to sell or resell this template in any form.
+
+  Credit and Donations: While not mandatory, I would greatly appreciate it if you could give me credit for the template. 
+  A small mention or link back to my website would be wonderful. 
+  If you find this template useful and would like to support my work, donations are also welcome.
+
+  Contact Information:
+    Name: Genial Ingele
+
+    Website: https://genialingele.github.io/
+
+    Email: genial@ndolena.com
+
+    Thank you for respecting these terms and for your support!
+*/
